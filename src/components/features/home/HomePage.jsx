d364fc9bb29bcdfae04c6dab0aaef16fb3fbabf1
Anan014 <ananamer95@gmail.com>
2021-11-04 11:40:35 +0200

    before intro

--- src/components/features/home/HomePage.jsx
+++ src/components/features/home/HomePage.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Container, Segment,Header,Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Container, Segment, Header, Icon, Button } from 'semantic-ui-react';
 
 export default function HomePage() {
     return (
         <Segment inverted textAlign='center' vertical className='masthead'>
             <Container>
-            <Header as='h1' inverted>
+                <Header as='h1' inverted>
                     <Icon size='massive' name='clipboard' style={{ marginBottom: 12 }} />
                     Skill Board
                 </Header>
+                <Button as={Link} exact to='/posts' />
             </Container>
         </Segment>
     );
