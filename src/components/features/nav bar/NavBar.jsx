49b3ec77c27d74069a974d7a900e01fe3385d08b
Anan014 <ananamer95@gmail.com>
2021-11-04 13:26:17 +0200

    before intro

--- src/components/features/nav bar/NavBar.jsx
+++ src/components/features/nav bar/NavBar.jsx
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
 import { Menu, Container, Icon } from "semantic-ui-react";
 import SignedInMenu from './SignInMenu';
 import SignedOutMenu from './SignOutMenu';
 
 export default function NavBar() {
     const [authenticated, setAuthenticated] = useState(true);
+    const history = useHistory();
 
     function handleSignOut() {
         setAuthenticated(false);
+        history.push('/');
     }
 
     return (
         <Menu inverted fixed='top'>
             <Container>
                 <Menu.Item as={NavLink} exact to='/' header>
-                <Icon name='clipboard outline'/>
+                    <Icon name='clipboard outline' />
                     SkillBoard
                 </Menu.Item>
                 <Menu.Item as={NavLink} exact to='/posts' header name='Posts' />
